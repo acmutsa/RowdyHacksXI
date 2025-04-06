d73a0b570ab5c38917fa4487b71ff70f2ed70cd7
joshuasilva414 <joshuasilva414@gmail.com>
2025-04-06 16:23:45 -0500

    motion demo

--- apps/web/src/components/landing/Hero.tsx
+++ apps/web/src/components/landing/Hero.tsx
@@ -1,55 +1,100 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
 import { Button } from "../shadcn/ui/button";
 
+import { motion, useScroll, useTransform } from "framer-motion";
+
 export default function Hero() {
+	const { scrollYProgress } = useScroll();
 	return (
 		<section className="grid w-full grid-cols-1 overflow-hidden">
-			<div className="relative flex min-h-screen w-full flex-col items-center justify-center">
-				<div className="absolute left-[20%] top-[-30vh] h-[110vh] w-[225px] -translate-x-5 -rotate-[50deg] bg-white opacity-20 blur-3xl"></div>
-				<div className="relative">
-					<div className="absolute h-full w-full rounded-3xl bg-hackathon opacity-10 blur-3xl"></div>
-					<div className="z-10 grid h-min grid-cols-2">
-						<div className="relative">
-							<Image
-								src="/img/logo/hackkit-md.png"
-								alt="HackKit Logo"
-								fill
-								className="object-contain"
-							/>
-						</div>
-						<div className="z-10 flex py-5">
-							<h1 className="text-7xl font-black text-hackathon dark:bg-gradient-to-tl dark:from-hackathon/80 dark:to-white dark:bg-clip-text dark:text-transparent md:text-8xl">
-								Hack
-								<br />
-								Kit
-							</h1>
-						</div>
-					</div>
-					<p className="text-md pl-5 pt-10 text-center font-bold text-muted-foreground">
-						Feature-packed Hackathon managment software{" "}
-						<u>that just works</u>.
-					</p>
+			<div className="night relative flex h-screen w-full flex-col justify-end">
+				<div className="relative z-20 w-screen">
+					<motion.div
+						style={{
+							translateY: useTransform(
+								scrollYProgress,
+								[0, 1],
+								[0, 10000],
+							),
+						}}
+					>
+						<Image
+							className="w-full"
+							src={"/img/sands.png"}
+							alt="Sands"
+							width={1000}
+							height={1000}
+							unoptimized={true}
+						/>
+						<Image
+							className="absolute bottom-[20%] left-[20%] z-20 w-[100px]"
+							src={"/img/tumbleweed.svg"}
+							alt="Tumbleweed"
+							width={100}
+							height={100}
+							unoptimized={true}
+						/>
+						<Image
+							className="absolute bottom-[15%] left-[60%] z-20 w-[80px]"
+							src={"/img/tumbleweed.svg"}
+							alt="Tumbleweed"
+							width={80}
+							height={80}
+							unoptimized={true}
+						/>
+					</motion.div>
 				</div>
-			</div>
-			<div className="absolute top-[80vh] flex w-full flex-wrap items-center justify-center gap-x-2 gap-y-4">
-				<Link href={"https://github.com/acmutsa/hackkit"}>
-					<Button variant={"outline"} size={"lg"}>
-						GitHub
-					</Button>
-				</Link>
-				<Link href={"https://github.com/acmutsa/hackkit"}>
-					<Button variant={"outline"} size={"lg"}>
-						Docs
-					</Button>
-				</Link>
-				<Link href={"https://github.com/acmutsa/hackkit"}>
-					<Button variant={"outline"} size={"lg"}>
-						Channel Log
-					</Button>
-				</Link>
-				<div className="h-0 basis-full" />
-				<div className="max-h-[50px] overflow-hidden"></div>
+				<motion.div
+					className="absolute right-[10%] top-[10%] z-20 w-[120px]"
+					style={{
+						translateX: useTransform(
+							scrollYProgress,
+							[0, 1],
+							[0, 1000],
+						),
+						rotate: useTransform(scrollYProgress, [0, 1], [0, 360]),
+					}}
+				>
+					<Image
+						className="absolute right-[10%] top-[10%] z-20 w-[120px]"
+						src={"/img/cow.svg"}
+						alt="Floating Cow"
+						width={120}
+						height={120}
+						unoptimized={true}
+					/>
+				</motion.div>
+				<motion.div
+					className="absolute left-1/2 top-1/2 w-full max-w-[1200px]"
+					style={{
+						translateX: useTransform(
+							scrollYProgress,
+							[0, 1],
+							["-50%", "-1000%"],
+						),
+						translateY: useTransform(
+							scrollYProgress,
+							[0, 1],
+							["-50%", "-100%"],
+						),
+						scale: useTransform(scrollYProgress, [0, 1], [1, 10]),
+						rotate: useTransform(
+							scrollYProgress,
+							[0, 1],
+							[0, -180],
+						),
+					}}
+				>
+					<Image
+						src={"/img/hero-title.svg"}
+						alt={"Rh Xi Hero Title"}
+						width={1000}
+						height={1000}
+						unoptimized={true}
+					/>
+				</motion.div>
 			</div>
 		</section>
 	);
--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -28,6 +28,7 @@ module.exports = {
 				ring: "hsl(var(--ring))",
 				background: "hsl(var(--background))",
 				foreground: "hsl(var(--foreground))",
+				"rh-night": "var(--rh-night)",
 				primary: {
 					DEFAULT: "hsl(var(--primary))",
 					foreground: "hsl(var(--primary-foreground))",

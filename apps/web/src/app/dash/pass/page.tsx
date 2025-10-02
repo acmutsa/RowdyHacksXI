2c15f88aec7022bb765da5eb3fc0bb0c137d506c
Carter LaVigne <carterlavigne3@gmail.com>
2025-10-02 14:35:29 -0500

    event pass star background

--- apps/web/src/app/dash/pass/page.tsx
+++ apps/web/src/app/dash/pass/page.tsx
@@ -51,7 +51,7 @@ function EventPass({ qrPayload, user, clerk, guild }: EventPassProps) {
 	return (
 		<div className="relative my-20 h-max">
 			<div className="absolute left-1/2 top-0 z-10 h-[75px] w-[75px] -translate-x-1/2 -translate-y-[50%] rotate-45 rounded-full border-2 border-background border-b-muted border-r-muted bg-background dark:border" />
-			<div className="flex aspect-[9/17] !max-h-[calc(100vh-7rem)] w-full max-w-[400px] flex-col overflow-hidden rounded-3xl border-2 border-muted bg-background py-[37.5px] dark:border">
+			<div className="flex aspect-[9/17] !max-h-[calc(100vh-7rem)] w-full max-w-[400px] flex-col overflow-hidden rounded-3xl border-2 border-muted star-bg py-[37.5px] dark:border">
 				<div className="relative flex h-[30%] w-full flex-col items-center pt-2">
 					<Image
 						src={clerk.imageUrl}

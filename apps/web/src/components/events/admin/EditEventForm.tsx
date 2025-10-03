e56dda1ddc78bccc298afd32f183b5094e63c64a
joshuasilva414 <joshuasilva414@gmail.com>
2025-10-03 11:50:50 -0500

    event form schema refactor

--- apps/web/src/components/events/admin/EditEventForm.tsx
+++ apps/web/src/components/events/admin/EditEventForm.tsx
@@ -30,7 +30,7 @@ import { useCallback, useState } from "react";
 import { useRouter } from "next/navigation";
 import { ONE_HOUR_IN_MILLISECONDS } from "@/lib/constants";
 import { type eventEditType } from "@/lib/types/events";
-import { newEventFormSchema } from "@/validators/event";
+import { editEventFormSchema } from "@/validators/event";
 import { ThreeCircles } from "react-loader-spinner";
 import { useAction } from "next-safe-action/hooks";
 import { editEvent } from "@/actions/admin/event-actions";
@@ -75,8 +75,8 @@ export default function EditEventForm({
 		},
 	});
 
-	const form = useForm<z.infer<typeof newEventFormSchema>>({
-		resolver: zodResolver(newEventFormSchema),
+	const form = useForm<z.infer<typeof editEventFormSchema>>({
+		resolver: zodResolver(editEventFormSchema),
 		defaultValues: {
 			id,
 			title,
@@ -91,9 +91,10 @@ export default function EditEventForm({
 	});
 
 	const onSubmit = useCallback(
-		(values: z.infer<typeof newEventFormSchema>) => {
+		(values: z.infer<typeof editEventFormSchema>) => {
 			if (form.formState.isDirty) {
-				execute(values);
+				const payload = editEventFormSchema.parse(values);
+				execute(payload);
 			} else {
 				toast.error("Failed to edit event", {
 					description:
@@ -101,7 +102,7 @@ export default function EditEventForm({
 				});
 			}
 		},
-		[form.formState.isDirty],
+		[form.formState.isDirty, execute],
 	);
 
 	return (
--- apps/web/src/validators/event.ts
+++ apps/web/src/validators/event.ts
@@ -4,20 +4,30 @@ import { events } from "db/schema";
 import z from "zod";
 import c from "config";
 
-export const newEventFormSchema = createInsertSchema(events, {
+const eventBaseSchema = createInsertSchema(events, {
 	title: z.string().min(1),
 	description: z.string().min(1),
 	startTime: z.date(),
 	endTime: z.date(),
 	host: z.string().max(255).nullable(),
 	type: z.enum(Object.keys(c.eventTypes) as EventTypeEnum),
-}).refine(({ startTime, endTime }) => startTime < endTime, {
-	message: "Start time must be before end time",
-	path: ["startTime"],
 });
 
-export const editEventFormSchema = newEventFormSchema.extend({
-	id: z.coerce.number().int().positive(),
-});
+export const newEventFormSchema = eventBaseSchema.refine(
+	({ startTime, endTime }) => startTime < endTime,
+	{
+		message: "Start time must be before end time",
+		path: ["startTime"],
+	},
+);
+
+export const editEventFormSchema = eventBaseSchema
+	.extend({
+		id: z.coerce.number().int().positive(),
+	})
+	.refine(({ startTime, endTime }) => startTime < endTime, {
+		message: "Start time must be before end time",
+		path: ["startTime"],
+	});
 
 export const eventDataTableValidator = createSelectSchema(events);
